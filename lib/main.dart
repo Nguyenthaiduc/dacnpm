c360d28d47442889424eeba9eaa7375a310f6654
nguyenthaiduc <nguyenthaiduc12@gmail.com>
2022-06-09 21:33:18 +0700

    Resetting the Quiz_

--- lib/main.dart
+++ lib/main.dart
@@ -17,21 +17,48 @@ class _MyAppState extends State<MyApp> {
   final _questions = const [
     {
       'questionText': 'What\'s is your favorite color ?',
-      'answers': ['Black', 'Red', 'Green', 'White '],
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 3},
+        {'text': 'White', 'score': 1}
+      ],
     }, // Map key:value,
     {
       'questionText': 'What\'s is your favorite animal ?',
-      'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion '],
+      'answers': [
+        {'text': 'Rabbit', 'score': 3},
+        {'text': 'Snake', 'score': 11},
+        {'text': 'Elephant', 'score': 5},
+        {'text': 'Lion', 'score': 9}
+      ],
     },
     {
       'questionText': 'Who\'s is your favorite istructor ?',
-      'answers': ['Max', 'Max', 'Max', 'Max '],
+      'answers': [
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1}
+      ],
     },
   ];
 
   var _questionIndex = 0;
+  var _totalScore = 0;
 
-  void _answerQuestion() {
+  void _resetQuiz(){
+    setState(() {
+    _questionIndex = 0;
+    _totalScore = 0;
+    });
+
+  }
+
+  void _answerQuestion(int score) {
+
+    _totalScore += score;
+
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
@@ -55,8 +82,8 @@ class _MyAppState extends State<MyApp> {
                 answerQuestion: _answerQuestion,
                 questionIndex: _questionIndex,
                 questions: _questions,
-                )
-            : Result(),
+              )
+            : Result(_totalScore,_resetQuiz),
       ),
     );
   }
--- lib/quiz.dart
+++ lib/quiz.dart
@@ -26,9 +26,9 @@ class Quiz extends StatelessWidget {
                   Question(
                     questions[questionIndex]['questionText'] as String,
                   ),
-                  ...(questions[questionIndex]['answers'] as List<String>)
-                      .map((answer) {
-                    return Answer(answerQuestion, answer);
+                  ...(questions[questionIndex]['answers'] as List<Map<String,Object>>)
+                      .map<Widget>((answer) {
+                    return Answer(() => answerQuestion(answer['score']), answer['text'] as String);
                   }).toList()
                 ],
               );
